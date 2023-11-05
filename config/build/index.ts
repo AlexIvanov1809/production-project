9628e1089b65d80169a3fce836fed8e22b79a618
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-11-05 15:56:20 +0300

    get config

---
+++ config/build/index.ts
@@ -0,0 +1,6 @@
+import { buildDevServer } from './buildDevServer';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolve } from './buildResolve';
+
+export { buildLoaders, buildPlugins, buildResolve, buildDevServer };
