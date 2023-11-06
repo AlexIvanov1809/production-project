964511d0abda9f6941e9803839ccdfb525a54336
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-11-06 14:32:20 +0300

    add Sidebar

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,17 +2,18 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
-import { Navbar } from 'widgets';
-import { ThemeSwitcher } from 'shared/ui';
+import { Navbar, Sidebar } from 'widgets';
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <ThemeSwitcher />
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,1 +1,2 @@
+export { Sidebar } from './Sidebar/ui/Sidebar/Sidebar';
 export { Navbar } from './Navbar/ui/Navbar';
