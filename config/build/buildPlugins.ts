ca0e946b7f4281f9560e721484928a610b4e5e2e
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-11-15 15:02:07 +0300

    fix dev plagins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -10,7 +10,7 @@ export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
@@ -22,9 +22,16 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    isDev ? new ReactRefreshPlugin() : undefined,
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshPlugin());
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        openAnalyzer: false,
+      }),
+    );
+  }
+
+  return plugins;
 }
