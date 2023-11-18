1f81e6351aeb2da710df394df4458b7d9c3fa1ad
AlexIvanov1809 <alexanderivanoff88@gmail.com>
2023-11-18 14:23:04 +0300

    fix global theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { Navbar, Sidebar } from 'widgets';
 import { Suspense } from 'react';
 
 const App = () => {
-  const { theme } = useTheme();
-
   return (
-    <div className={classNames('app', {}, [theme])}>
+    <div className={classNames('app', {}, [])}>
       <Suspense fallback="">
         <Navbar />
         <div className="content-page">
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -8,7 +8,6 @@ import {
   useCallback,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
-import { useTheme } from 'app/providers/ThemeProvider';
 
 interface ModalProps {
   className?: string;
@@ -22,7 +21,6 @@ const ANIMATION_DELAY = 300;
 export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
   const [isClosing, setIsClosing] = useState(false);
   const timerRef = useRef<ReturnType<typeof setTimeout>>();
-  const { theme } = useTheme();
   const mods: Record<string, boolean> = {
     [cls.opened]: isOpen,
     [cls.isClosing]: isClosing,
@@ -62,7 +60,7 @@ export const Modal = ({ className, children, isOpen, onClose }: ModalProps) => {
   }, [isOpen, onKeyDown]);
   return (
     <Portal>
-      <div className={classNames(cls.Modal, mods, [className, theme])}>
+      <div className={classNames(cls.Modal, mods, [className])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div className={cls.content} onClick={onContentClick}>
             {children}
